38f31598d9f80cdd843c1eaf129d65d87cf26320
ujigis <kazu013057@gmail.com>
2017-01-21 15:39:05 +0900

    appを修正
    appを修正

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('Ikoma_sample_matsuda.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://ujigis.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));
